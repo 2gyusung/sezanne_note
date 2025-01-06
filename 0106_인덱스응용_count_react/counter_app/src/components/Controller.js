27823dd77a6c91390997bbbffefefbf691568fed
2gyusung <leegs005@naver.com>
2025-01-06 16:57:34 +0900

    0106일자 index응용 react counter

--- 0106_인덱스응용_count_react/counter_app/src/components/Controller.js
+++ 0106_인덱스응용_count_react/counter_app/src/components/Controller.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 function Controller({handleCount}) {
-  // const onClickCnt = (e) => { 방법2
-  //   const value = Number(e.target.value)
-  //   handleCount(value)
-  // }
+  const onClickCnt = (e) => { //방법2
+    const value = Number(e.target.value)
+    handleCount(value)
+  }
   return (
     <div className='Controller'>
       <button onClick={()=> handleCount(-1)}>-1</button>
-      <button onClick={()=> handleCount(-10)}>-10</button>
+      <button value={-10} onClick={onClickCnt}>-10</button>  {/*방법2*/}
       <button onClick={()=> handleCount(-100)}>-100</button>
       <button onClick={()=> handleCount(100)}>+100</button>
       <button onClick={()=> handleCount(10)}>+10</button>
